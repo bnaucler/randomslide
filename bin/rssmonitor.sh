18c1b186fea686395449a6960547df4b215e4059
bnaucler <mail@bnaucler.se>
2020-02-11 21:16:00 +0100

    Adding server monitor script

---
+++ bin/rssmonitor.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+INTERVAL=30
+LOGFILE="data/rsserver.log"
+
+while true; do
+    git pull
+    PID=`cat data/rsserver.pid`
+    ISUP=`ps -p $PID | wc -l`
+    if [ $ISUP == "1" ]; then
+        DATE=`date +'%a | %Y-%m-%d | %R:%S'`
+        echo "$DATE: Restarting server"
+        nohup bin/rsserver -v > $LOGFILE&
+    fi
+    sleep $INTERVAL
+done
