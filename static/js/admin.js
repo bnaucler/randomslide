5f059613057dda0fb02095e41dcfc74fd7cb3e84
amesteDev <info@ameste.se>
2020-02-12 21:18:18 +0100

    working on admin-page

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,8 +1,11 @@
-//funktion att nå loggarna
-//loggarna kommer finnas i /log
-
-
 function restartServer(){
     var xh = new XMLHttpRequest();
     xhr.open('GET', "/request", true);
+}
+
+
+function fetchLogs(){
+    //something, something
+    //funktion att nå loggarna
+//loggarna kommer finnas i /log
 }
