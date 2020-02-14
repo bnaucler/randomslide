bc19273b9e1037de3f6494473ce939111353471b
bnaucler <mail@bnaucler.se>
2020-02-14 21:02:43 +0100

    Improved (?) monitoring

--- bin/rsmonitor.sh
+++ bin/rsmonitor.sh
@@ -5,17 +5,22 @@ PIDFILE="data/rsserver.pid"
 RSLOGFILE="static/log/rsserver.log"
 MONLOGFILE="static/log/rsmonitor.log"
 
-touch $PIDFILE $RSLOGFILE $MONLOGFILE
+startserver() {
+    bin/build.sh >> $MONLOGFILE
+    DATE=`date +'%a | %Y-%m-%d | %R:%S'`
+    echo "$DATE: Restarting server" >> $MONLOGFILE
+    nohup bin/rsserver -v >> $MONLOGFILE &
+}
 
 while true; do
     git pull
+
+    if [ ! -f $PIDFILE ]; then startserver; fi
+
     PID=`cat $PIDFILE`
     kill -0 $PID > /dev/null
-    if [ $? -eq 1 ]; then
-        bin/build.sh >> $MONLOGFILE
-        DATE=`date +'%a | %Y-%m-%d | %R:%S'`
-        echo "$DATE: Restarting server" >> $MONLOGFILE
-        nohup bin/rsserver -v >> $MONLOGFILE &
-    fi
+
+    if [ $? -eq 1 ]; then startserver; fi
+
     sleep $INTERVAL
 done
