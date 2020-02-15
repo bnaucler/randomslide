07a170104b402db53254615e7bba98255ebee09c
amesteDev <info@ameste.se>
2020-02-15 15:36:25 +0100

    generated slides can be shown and changed with buttons. (no connection to backend or choices made yet)

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,8 @@
+var slideProg;
+var timer;
+var resp;
+var slideIndex = 1;
+
 document.getElementById('timerOrNot').addEventListener('change', function() {
     if(this.value === "timer"){
         document.getElementById("slideTimer").style.display = "inline";
@@ -5,9 +10,6 @@ document.getElementById('timerOrNot').addEventListener('change', function() {
         document.getElementById("slideTimer").style.display = "none";
     }
   });
-
-//fetching slides as json with ajax
-var resp;
 
 function fetchSlides(){
     var xhttp = new XMLHttpRequest();
@@ -25,12 +27,13 @@ function fetchSlides(){
 // creating slides from the JSON 
 function createSlides(resp){
     for(i in resp.slides){
-        console.log(resp.slides[i]);
         let outputEl = document.getElementById("output");
         let div = document.createElement("div");
+        div.setAttribute("class", "theSlides");
         div.style.display = "none";
         div.style.background = resp.slides[i].bgcolor;
         outputEl.appendChild(div);
+
         let slideHeader = document.createElement("h2");
         let headerText = document.createTextNode(resp.slides[i].title);
         slideHeader.style.color = resp.slides[i].tcolor;
@@ -46,40 +49,70 @@ function createSlides(resp){
         slideTxt.style.color = resp.slides[i].tcolor;
         slideTxt.appendChild(slideContent);
         div.appendChild(slideTxt);
-    
     }
-    
-    setTimeout(displaySlides, 5000);
+    setTimeout(loadingSlides, 1000);
 }
 
-function displaySlides(){
-    document.getElementById("formwrapper").innerHTML = "";
+
+
+
+function loadingSlides(){
+    let amount = document.getElementById("amountOfSlides").value;
+    let category = document.getElementById("category").value;
+    let lang = document.getElementById("lang").value;
+    let wrapper = document.getElementById("formwrapper");
+    slideProg = document.getElementById("timerOrNot").value;
+    timer = document.getElementById("time").value;
+
+    wrapper.innerHTML = "";
+    wrapper.innerHTML += "Your categroy:  " + category + "<br />";
+    wrapper.innerHTML += "Amount of slides: " + amount + "<br />";
+    if(slideProg == "change"){
+        wrapper.innerHTML += "Your choice is to change slides yourself. <br />"
+    } else {
+        wrapper.innerHTML += "Your choice is that slides change every " + timer + " seconds. <br />";
+    }
+    wrapper.innerHTML += "Language: " + lang + "<br />";
+
     let butt = document.createElement("button");
     let buttxt = document.createTextNode("GO!");
     butt.setAttribute("class", "bigredbutton");
+    butt.setAttribute("onclick", "slideShow()");
     butt.appendChild(buttxt);
     document.getElementById("formwrapper").appendChild(butt);
-    let div = document.createElement("div");
-    let p = document.createElement("p");
-    
-    //amountofslides
 
-    //klicka själv eller byta automagi
-    //Om automagi: hur lång tid?
-
-    //ladda in CSS
-
-
-    //språk
-
-
-    //kategori
+}
 
 
 
+function slideShow(n){
+    if(slideProg === "change"){
+        document.getElementById("prev").style.display = "inline";
+        document.getElementById("next").style.display = "inline";
+    } else {
+        console.log(timer * 1000, changeSlide);
+        changeSlide()
+    }
 
-        //hämta om timer eller göra själv
-        //om timer så byt av sig själv
-        //om göra själv så lägg in knappar att byta slide med
-        //slidenummer nere till höger i format 2/10.
+    let slides = document.getElementsByClassName("theSlides");
+    if(n > slides.length){
+        slideIndex = 1;
+        console.log("Slut på bilder, lägg in en end screen eller något");
+    }
+    if(n < 1){
+        slideIndex = slides.length;
+    }
+    for(let i = 0; i < slides.length; i++){
+        slides[i].style.display = "none"; 
+    }
+    slides[slideIndex-1].style.display = "block"; 
+
+}
+
+function changeSlide(n){
+    slideShow(slideIndex += n);
+}
+
+function displayTimer(time){
+    setTimeout(1000, displayTimer());
 }
