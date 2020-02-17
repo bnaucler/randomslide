d5837d71096a999b9fde1a6d9b7fb2cb2a594c8a
bnaucler <mail@bnaucler.se>
2020-02-17 21:28:13 +0100

    Adding dumping of tag lists

--- tools/dbdump.go
+++ tools/dbdump.go
@@ -30,6 +30,23 @@ func retrtxt(db *bolt.DB, mxind int, buc []byte) []rscore.Textobj {
     return ret
 }
 
+func gettaglist(db *bolt.DB, tl []string, buc []byte) []rscore.Tag {
+
+    var ret []rscore.Tag
+    ctag := rscore.Tag{}
+
+    for _, t := range tl {
+        k := []byte(t)
+        v, e := rsdb.Rdb(db, k, buc)
+        rscore.Cherr(e)
+
+        json.Unmarshal(v, &ctag)
+        ret = append(ret, ctag)
+    }
+
+    return ret
+}
+
 func main() {
 
     if len(os.Args) < 2 { os.Exit(1) }
@@ -43,13 +60,22 @@ func main() {
     // DUMP SETTINGS
     fmt.Printf("SETTINGS: %+v\n", settings)
 
+    // DUMP TAG LISTS
+    ttl := gettaglist(db, settings.Taglist, rscore.TBUC)
+    fmt.Printf("TTEXT TAG LIST: %+v\n", ttl)
+
+    btl := gettaglist(db, settings.Taglist, rscore.BBUC)
+    fmt.Printf("BTEXT TAG LIST: %+v\n", btl)
+
     // DUMP TTEXT
-    ttext := retrtxt(db, settings.Tmax, rscore.TBUC)
-    fmt.Printf("TTEXT: %+v\n", ttext)
+    if settings.Tmax > 0 {
+        ttext := retrtxt(db, settings.Tmax, rscore.TBUC)
+        fmt.Printf("TTEXT: %+v\n", ttext)
+    }
 
     // DUMP BTEXT
-    btext := retrtxt(db, settings.Bmax, rscore.BBUC)
-    fmt.Printf("BTEXT: %+v\n", btext)
-
-
+    if settings.Bmax > 0 {
+        btext := retrtxt(db, settings.Bmax, rscore.BBUC)
+        fmt.Printf("BTEXT: %+v\n", btext)
+    }
 }
