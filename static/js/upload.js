306e4a6f8a584b45b99dad4241e985a033470ebc
amesteDev <info@ameste.se>
2020-02-20 17:49:04 +0100

    uploading fixed with ajax

--- static/js/upload.js
+++ static/js/upload.js
@@ -29,22 +29,27 @@ function addImg(){
     let url = "/addimg?tags=" + imgtags;
     let form = document.getElementById("imgForm");
 
+    form.onsubmit = function(){
+        let formdata = new FormData();
+        let fileToSend = document.getElementById("file");
+        formdata.append('file', fileToSend);
+        var imgJX = new XMLHttpRequest();
+        imgJX.open('POST', url, true);
+        imgJX.send(formdata);
 
-    form.addEventListener('submit', e =>{
-        e.preventDefault();
-        let file = document.getElementById("file").file;
-        let formData = new FormData();
-        let myHeaders = new Headers();
-        myHeaders.append('enctype', 'multipart/form-data');
-
-        formData.append(file);
-
-        fetch(url, {
-            method: 'POST',
-            headers: myHeaders,
-            body: formData,
-          }).then(response => {
-            console.log(response);
-          })
-    });
+    }
+    imgJX.onreadystatechange = function(){
+        if (this.readyState == 4){
+            let resp = JSON.parse(this.responseText);
+            if(resp.Code == 0){
+                var alertHTML = '<div class="alert">Success!</div>';
+                document.body.insertAdjacentHTML("beforeend", alertHTML);
+                setTimeout(() => document.querySelector('.alert').outerHTML = "", 2000);
+            }else {
+                var alertHTML = '<div class="alert">Something went wrong! </div>';
+                document.body.insertAdjacentHTML("beforeend", alertHTML);
+                setTimeout(() => document.querySelector('.alert').outerHTML = "", 2000);
+            }
+        }
+    }
 }
