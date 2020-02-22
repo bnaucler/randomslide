5d7f877d0d79b0116ad1a9a3c1cec79f8f2e7cc2
bnaucler <mail@bnaucler.se>
2020-02-22 13:29:03 +0100

    Adding monitor cli arg to set port

--- bin/rsmonitor.sh
+++ bin/rsmonitor.sh
@@ -4,18 +4,31 @@ INTERVAL=30
 PIDFILE="data/rsserver.pid"
 RSLOGFILE="static/log/rsserver.log"
 MONLOGFILE="static/log/rsmonitor.log"
+PORT=6291
 
 startserver() {
     bin/build.sh all >> $MONLOGFILE
     DATE=`date +'%a | %Y-%m-%d | %R:%S'`
     echo "$DATE: Restarting server" >> $MONLOGFILE
-    nohup bin/rsserver -v >> $MONLOGFILE &
+    nohup bin/rsserver -v -p $1 >> $MONLOGFILE &
 }
+
+usage() {
+    echo "-p <port> - listen on port (default: $PORT)"
+    exit 0
+}
+
+while getopts 'hp:' flag; do
+    case "${flag}" in
+        h) usage ;;
+        p) PORT="${OPTARG}";;
+    esac
+done
 
 while true; do
     git pull
 
-    if [ ! -f $PIDFILE ]; then startserver; fi
+    if [ ! -f $PIDFILE ]; then startserver $PORT; fi
 
     PID=`cat $PIDFILE`
     kill -0 $PID > /dev/null
