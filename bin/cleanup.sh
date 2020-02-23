efc407f6a4a734c6a39fd74c9a70622aefd24919
bnaucler <mail@bnaucler.se>
2020-02-23 21:04:57 +0100

    Including database removal

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -2,16 +2,18 @@
 
 SPIDFILE="data/rsserver.pid"
 MPIDFILE="data/rsmonitor.pid"
+DBFILE="data/rs.db"
 SERVER="localhost"
 PORT=6291
 
 usage() {
     echo "$0 usage:"
     echo "-h: this message"
+    echo "-d: delete database file ($DBFILE)"
     echo "-f <arg>: specify server pidfile Location (default: $SPIDFILE)"
     echo "-m: SIGQUIT monitor"
     echo "-s <arg>: specify server address (default: $SERVER)"
-    echo "-r: remove server pidfile"
+    echo "-r: remove server pidfile ($SPIDFILE)"
     echo "-k: SIGQUIT server"
     echo "-x: SIGKILL all servers"
     echo "-p <arg>: set port (default: $PORT)"
@@ -23,8 +25,9 @@ skill() {
     kill -9 $PID
 }
 
-while getopts 'hfmrkp:xs' flag; do
+while getopts 'dhfmrkp:xs' flag; do
     case "${flag}" in
+        d) rm $DBFILE ;;
         h) usage ;;
         f) SPIDFILE="${OPTARG}" ;;
         r) rm $SPIDFILE $MPIDFILE;;
