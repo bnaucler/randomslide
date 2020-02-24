6bde4d269ea663403b7c5ad55e59e33f37ae8a8a
bnaucler <mail@bnaucler.se>
2020-02-24 11:53:25 +0100

    Future-proofing build script

--- bin/build.sh
+++ bin/build.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+TOOLS=("dbdump")
+
 echo "Building rsserver"
 go build -o bin/rsserver rsserver/rsserver.go
 
 if [ "$1" = "all" ]; then
-    echo "Building tools"
-    go build -o bin/dbdump tools/dbdump.go
+    for FN in $TOOLS; do
+        echo "Building $FN"
+        go build -o bin/$FN tools/$FN.go
+    done
 fi
