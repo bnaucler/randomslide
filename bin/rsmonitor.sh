ac607100648d9f4f0977ee98e97c7d8094d9a8c7
bnaucler <mail@bnaucler.se>
2020-02-29 11:52:23 +0100

    Adding volatile switch to monitor

--- bin/rsmonitor.sh
+++ bin/rsmonitor.sh
@@ -5,17 +5,19 @@ SPIDFILE="data/rsserver.pid"
 MPIDFILE="data/rsmonitor.pid"
 RSLOGFILE="static/log/rsserver.log"
 MONLOGFILE="static/log/rsmonitor.log"
+VSWITCH=""
 PORT=6291
 
 startserver() {
     bin/build.sh all >> $MONLOGFILE
     DATE=`date +'%a | %Y-%m-%d | %R:%S'`
     echo "$DATE: Restarting server" >> $MONLOGFILE
-    nohup bin/rsserver -x -v -p $1 >> $MONLOGFILE &
+    nohup bin/rsserver $VSWITCH -v -p $1 >> $MONLOGFILE &
 }
 
 usage() {
-    echo "-p <port> - listen on port (default: $PORT)"
+    echo "-p <port>: listen on port (default: $PORT)"
+    echo "-x: Start server in volatile mode"
     exit 0
 }
 
@@ -23,10 +25,11 @@ cleanup() {
     rm $MPIDFILE
 }
 
-while getopts 'hp:' flag; do
+while getopts 'hp:x' flag; do
     case "${flag}" in
         h) usage ;;
         p) PORT="${OPTARG}";;
+        x) VSWITCH="-x"
     esac
 done
 
