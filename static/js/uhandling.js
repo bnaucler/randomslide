8717bf9cf478b1ac4807356b738af258d024549d
amesteDev <info@ameste.se>
2020-03-06 18:59:20 +0100

    first draft of register-system needs polish

---
+++ static/js/uhandling.js
@@ -0,0 +1,34 @@
+function registerUser(){
+    let registerAjax = new XMLHttpRequest();
+    let userName = document.getElementById("username").value;
+    let passWord = document.getElementById("password").value;
+
+    registerAjax.onreadystatechange = function () {
+        if (this.readyState == 4 ) {
+            if (this.responseText.Code === 3){
+                window.alert(this.responseText.Text + ". Try again with another username.");
+            } else {
+                window.alert("User created, you are now logged in.")
+                sessionStorage.setItem('key', this.responseText.Text.Skey);
+                sessionStorage.setItem('user', userName);
+            }
+        }
+        console.log(sessionStorage.getItem("key"));
+        console.log(sessionStorage.getItem("user"));
+    }
+}
+registerAjax.open("POST", "/register?user=" + userName + "&pass=" + passWord, false);
+registerAjax.send();
+
+
+
+let loginAjax = new XMLHttpRequest();
+
+loginAjax.open("POST", "/login?user=" + userName + "&pass=" + passWord, false);
+loginAjax.send();
+
+
+let fbAjax = new XMLHttpRequest();
+
+fbAjax.open("POST", "/feedback?fb=" + feedback, false);
+fbAjax.send();
