9744d52694613d1f9b0fe8b2fac8ecfa6b62e654
amesteDev <info@ameste.se>
2020-03-06 19:09:43 +0100

    register user should now work

--- static/js/uhandling.js
+++ static/js/uhandling.js
@@ -6,18 +6,17 @@ function registerUser(){
     registerAjax.onreadystatechange = function () {
         if (this.readyState == 4 ) {
             let resp = JSON.parse(this.responseText);
-            console.log(resp);
-            if (this.responseText.Code === 3){
+
+            if (resp.Code === 3){
                 window.alert(resp.Text + ". Try again with another username.");
             } else {
                 window.alert("User created, you are now logged in.")
                 sessionStorage.setItem('key', resp.Skey);
-                sessionStorage.setItem('user', userName);
+                sessionStorage.setItem('user', resp.Name);
             }
 
         }
-        console.log(sessionStorage.getItem("key"));
-        console.log(sessionStorage.getItem("user"));
+
     }
     registerAjax.open("POST", "/register?user=" + userName + "&pass=" + passWord, false);
     registerAjax.send();
