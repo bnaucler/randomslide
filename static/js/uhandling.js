d4911361f3ead0972fe2a401135eab89b76208f3
amesteDev <info@ameste.se>
2020-03-06 19:27:22 +0100

    now login should work as intended

--- static/js/uhandling.js
+++ static/js/uhandling.js
@@ -25,12 +25,18 @@ function loginUser(){
     let loginAjax = new XMLHttpRequest();
     let userName = document.getElementById("username").value;
     let passWord = document.getElementById("password").value;
-
-    if (this.readyState == 4 ) {
-        let resp = JSON.parse(this.responseText);
-        console.log(resp);
+    registerAjax.onreadystatechange = function () {
+        if (this.readyState == 4 ) {
+            let resp = JSON.parse(this.responseText);
+            if (resp.Skey != ""){
+                window.alert("Logged in!")
+                sessionStorage.setItem('key', resp.Skey);
+                sessionStorage.setItem('user', resp.Name);
+            } else {
+                window.alert(resp.Text + "Wrong password or username!");
+            }
+        }
     }
-
     loginAjax.open("POST", "/login?user=" + userName + "&pass=" + passWord, false);
     loginAjax.send();
 
