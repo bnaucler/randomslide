658238083a1f2b8ec2cd32d1103e90ff46012919
amesteDev <info@ameste.se>
2020-03-06 19:16:19 +0100

    logins should now also work

--- static/js/uhandling.js
+++ static/js/uhandling.js
@@ -6,7 +6,6 @@ function registerUser(){
     registerAjax.onreadystatechange = function () {
         if (this.readyState == 4 ) {
             let resp = JSON.parse(this.responseText);
-
             if (resp.Code === 3){
                 window.alert(resp.Text + ". Try again with another username.");
             } else {
@@ -14,9 +13,7 @@ function registerUser(){
                 sessionStorage.setItem('key', resp.Skey);
                 sessionStorage.setItem('user', resp.Name);
             }
-
         }
-
     }
     registerAjax.open("POST", "/register?user=" + userName + "&pass=" + passWord, false);
     registerAjax.send();
@@ -24,12 +21,27 @@ function registerUser(){
 
 
 
+function loginUser(){
+    let loginAjax = new XMLHttpRequest();
+    let userName = document.getElementById("username").value;
+    let passWord = document.getElementById("password").value;
+
+    if (this.readyState == 4 ) {
+        let resp = JSON.parse(this.responseText);
+        if (resp.Skey != ""){
+            window.alert("Logged in!")
+            sessionStorage.setItem('key', resp.Skey);
+            sessionStorage.setItem('user', resp.Name);
+        } else {
+            window.alert(resp.Text + "Wrong password or username!");
+        }
+    }
+
+    loginAjax.open("POST", "/login?user=" + userName + "&pass=" + passWord, false);
+    loginAjax.send();
+
+}
 /*
-let loginAjax = new XMLHttpRequest();
-
-loginAjax.open("POST", "/login?user=" + userName + "&pass=" + passWord, false);
-loginAjax.send();
-
 
 let fbAjax = new XMLHttpRequest();
 
