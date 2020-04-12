fa8a6b3a91887ee712dcda11da8b95eb4e75b7d6
bnaucler <mail@bnaucler.se>
2020-04-12 20:30:54 +0200

    Implementing mkxhr() for text submissions

--- static/js/upload.js
+++ static/js/upload.js
@@ -1,39 +1,49 @@
-function addTitle(){
+// Returns user & skey string
+function getukstr() {
+    let user = sessionStorage.getItem('user');
+    let key = sessionStorage.getItem('key');
+    str = "&user=" + user + "&skey=" + key;
+
+    return str
+}
+
+// Processes text addition response
+function atresp(resp) {
+
+    let s = JSON.parse(resp.responseText);
+
+    console.log(s);
+    if(s.Code == 0) {
+        sendalert("Thank you for your submission");
+
+    } else {
+        sendalert(s.Text);
+    }
+}
+
+// Sends XHR requests to add text
+function addText() {
     let title = document.getElementById("titelinput").value;
     let body = document.getElementById("textinput").value;
     let tags = document.getElementById("taginput").value;
-    titleajax = new XMLHttpRequest();
-    titleajax.open('POST', "addtext?ttext=" + title + "&btext=" + body + "&tags=" + tags, true);
-    //addtext?= + title=kenneth&tags=kenneth apansson beer
-    titleajax.send();
 
-    titleajax.onreadystatechange = function() {
-    if (this.readyState == 4){
-            let resp = JSON.parse(this.responseText);
-            if(resp.Code == 0){
-                var alertHTML = '<div class="alert">Success!</div>';
-                document.body.insertAdjacentHTML("beforeend", alertHTML);
-                setTimeout(() => document.querySelector('.alert').outerHTML = "", 2000);
-            }else {
-                var alertHTML = '<div class="alert">Something went wrong! </div>';
-                document.body.insertAdjacentHTML("beforeend", alertHTML);
-                setTimeout(() => document.querySelector('.alert').outerHTML = "", 2000);
-            }
-        }
-    }
+    var req = "/addtext?ttext=" + title + "&btext=" + body + "&tags=" + tags + getukstr();
+    console.log(req);
+
+    mkxhr(req, atresp);
 }
 
 function addImg(){
     let imgtags = document.getElementById("imgTagInput").value;
     let url = "/addimg?tags=" + imgtags;
-    
+
     let formdata = new FormData();
     let fileToSend = document.getElementById("file").files[0];
     formdata.append('file', fileToSend);
     var imgJX = new XMLHttpRequest();
     imgJX.open('POST', url, true);
     imgJX.send(formdata);
-    
+
 
     imgJX.onreadystatechange = function(){
         if (this.readyState == 4){
